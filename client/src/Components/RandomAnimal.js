3ea778730f84bb163185a5d42b93ce57d5c9ad56
barndogghxc <barndogghxc@yahoo.com>
2018-03-20 10:19:20 -0400

    twitter sharing functionaltiy

--- client/src/Components/RandomAnimal.js
+++ client/src/Components/RandomAnimal.js
@@ -199,6 +199,8 @@ class RandomAnimal extends Component {
 
 				</div>
 				<button onClick={this.newAnimal}>New Pet</button>
+				<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="Whats shaking? This pets tail! What you think?" data-hashtags="petsy" data-show-count="false">Tweet</a>
+				<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><br/>
 			</div>
 			)
 	}
