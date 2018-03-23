2f8a55d601e4123c38c1cd6f8a08437a5935c60e
Hani Zahra <hanizahra@ool-ad0201eb.dyn.optonline.net>
2018-03-23 10:33:20 -0400

    solved conflicts between upstream and stash for jumbotron

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import Social from '../Social/Social';
-
-
 import ShareButton from 'react-social-share-buttons';
 import { PageHeader, Jumbotron, Button} from 'react-bootstrap';
-
 
 class Homepage extends Component {
   render() {
     return (
-      <div>
-        <h1>Homepage</h1>
-        <p>Welcome, find random animals to adopt!</p><br/>
-        <Social />
         <div>
             <Jumbotron bsClass = 'jumbotron'>
                 <h1>Petsy</h1>
@@ -23,25 +16,24 @@ class Homepage extends Component {
                     <Button bsStyle="primary" href='/results'>Start Your Search!</Button>
 
                 </p>
-            </Jumbotron>;
-        <ShareButton
+            </Jumbotron>
+
+            <ShareButton
                 compact
                 socialMedia={'facebook'}
                 url={"https://brian-martinez-portfolio.herokuapp.com/index.html"}
                 media={"https://brianmartinezjtm.com"}
                 text="adobt this cat"
             />
-        <ShareButton
+            
+            <ShareButton
                 compact
                 socialMedia={'twitter'}
                 url={"https://brian-martinez-portfolio.herokuapp.com/index.html"}
                 media={"https://brianmartinezjtm.com"}
                 text="adopt this dog"
-            />
+            />    
       </div>
-
-        
-
     )
   }
 }
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -3,47 +3,10 @@ import  './navigation.css'
 import { Link } from 'react-router-dom';
 import { Collapse, NavbarToggler, NavbarBrand,  NavLink } from 'reactstrap';
 import { Button, MenuItem, NavDropdown } from 'react-bootstrap';
-
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
 
 class Navigation extends Component {
   render() {
-		return(
-			<Navbar dark expand="md" color="dark">
-				<NavbarBrand href="/" className="mr-auto">Petsy</NavbarBrand>
-				<NavbarToggler onClick={this.toggle} />
-	      		<Collapse isOpen={this.state.isOpen} navbar>
-	        		<Nav className="ml-auto" navbar>
-	          			<NavItem>
-	            			<NavLink href='/'>Home</NavLink>
-	          			</NavItem>
-						<NavItem>
-	            			<NavLink href='/randomanimal'>Random Animal</NavLink>
-	          			</NavItem>
-	          			<NavItem>
-	            			<NavLink href='/results'>Animal Search</NavLink>
-	          			</NavItem>
-						<NavItem>
-	            			<NavLink href="/petslist">Pets List</NavLink>
-	          			</NavItem>
-						<NavItem>
-	            			<NavLink href="/quiz">Quiz</NavLink>
-	          			</NavItem>
-	        		</Nav>
-	      		</Collapse>
-			</Navbar>
-		)
-		// <nav>
-    //   <ul>
-		//     <li className='f3 link dim black underline pa3 pointer'><Link to='/'>Home</Link></li>
-    //     <li className='f3 link dim black underline pa3 pointer'><Link to='/randomanimal'>Random Animal</Link></li>
-		// 		<li className='f3 link dim black underline pa3 pointer'><Link to='/results'>Animal Search</Link></li>
-    //     <li className='f3 link dim black underline pa3 pointer'><Link to="/petslist">Pets List</Link></li>
-		// 		<li className='f3 link dim black underline pa3 pointer'><Link to="/quiz">Quiz</Link></li>
-    //   </ul>
-    // </nav>
-	}
-
     return (
       <Navbar inverse collapseOnSelect>
         <Navbar.Header>
@@ -66,9 +29,6 @@ class Navigation extends Component {
       </Navbar>
     )
   }
-
-
-
-
+}
 
 export default Navigation
